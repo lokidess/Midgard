45d672369dede3e63981d3c02ec37797f4097c45
bohdan.bilokon@raccoongang.com <bohdan.bilokon@raccoongang.com>
2019-08-22 23:23:48 +0300

    add user application

--- midgard/settings.py
+++ midgard/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'debug_toolbar',
+    'apps.user_profile',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'midgard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,  '../templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
