00c593a574019f52f9c36cd00793573c81505273
bohdan.bilokon@raccoongang.com <bohdan.bilokon@raccoongang.com>
2019-08-23 07:13:00 +0300

    change verbose name

--- apps/user_profile/models.py
+++ apps/user_profile/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class UserModel(AbstractUser):
 
-    first_name = models.CharField(verbose_name="Ім'я", max_length=30, blank=True)
-    middle_name = models.CharField(verbose_name='По батькові', max_length=30, blank=True)
-    last_name = models.CharField(verbose_name='Прізвище', max_length=30, blank=True)
+    first_name = models.CharField(verbose_name="Имя", max_length=30, blank=True)
+    middle_name = models.CharField(verbose_name='Отчество', max_length=30, blank=True)
+    last_name = models.CharField(verbose_name='Фамилия', max_length=30, blank=True)
     date_birth = models.DateField(verbose_name='День рождения', blank=True, null=True)
 
     def __str__(self):
