4d8c94a8f5e22ff97139922078743cb0f30c66e5
bohdan.bilokon@raccoongang.com <bohdan.bilokon@raccoongang.com>
2019-08-24 13:53:09 +0300

    add base user profile

--- midgard/settings.py
+++ midgard/settings.py
@@ -129,3 +129,9 @@ AUTH_USER_MODEL = 'user_profile.UserModel'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,  'static/'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR,  'static_files/')
